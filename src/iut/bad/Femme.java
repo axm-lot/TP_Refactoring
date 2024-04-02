d169272ef8815580fe81df1e96a2a239cd4de87f
charlotaxm@gmail.com <charlotaxm@gmail.com>
2024-04-02 16:48:36 +0300

    feature : creating Femme class

---
+++ src/iut/bad/Femme.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iut.bad;
+
+/**
+ *
+ * @author axm
+ */
+public class Femme {
+    String nom, prenom;
+    int age;
+    
+    public Femme(){}
+    
+    public Femme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+    
+    
+}
